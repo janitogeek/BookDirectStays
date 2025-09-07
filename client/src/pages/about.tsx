1baae9dcb98ffca784bae8a6510dd55f4d2abb38
Utilizador <Utilizador@Jans-MacBook-Air.local>
2025-09-07 22:18:57 +0200

    feat: make background images darker with 60% overlay

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -52,7 +52,7 @@ export default function About() {
           backgroundImage: 'url(/about-page-background.png)'
         }}
       >
-        <div className="absolute inset-0 bg-black/40"></div>
+        <div className="absolute inset-0 bg-black/60"></div>
         <div className="relative container mx-auto px-4 sm:px-6 lg:px-8 py-20 lg:py-32">
           <div className="max-w-4xl mx-auto text-center text-white">
             <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold mb-6 leading-tight">
--- client/src/pages/find-host.tsx
+++ client/src/pages/find-host.tsx
@@ -79,7 +79,7 @@ export default function FindHost() {
         }}
       >
         {/* Overlay for better text readability */}
-        <div className="absolute inset-0 bg-black/40"></div>
+        <div className="absolute inset-0 bg-black/60"></div>
         
         <div className="container mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
           <div className="max-w-4xl mx-auto text-center">
